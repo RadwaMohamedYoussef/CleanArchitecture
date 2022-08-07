acbee3d6c63f8eaf1110902b50bb9e58b67bb606
RadwaMohamedYoussef <102368675+RadwaMohamedYoussef@users.noreply.github.com>
2022-08-07 07:34:58 +0200

    Commit Service and Repository,CourseViewModels

---
+++ CleanArch/CleanArch.Appliction/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Appliction.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Appliction.Interfaces
+{
+   public interface ICourseService
+    {
+        IEquatable<CourseViewModel> GetCourses(); 
+    }
+}
---
+++ CleanArch/CleanArch.Appliction/ViewModels/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Appliction.ViewModels
+{
+ public   class CourseViewModel
+    {  IEquatable<Course> Courses { get; set; }
+    }
+}
